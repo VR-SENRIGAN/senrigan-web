a016d1f1977e2bcc96c3b2242e8dcac43dcee928
kunou <t-kunou@esm.co.jp>
2018-11-07 19:42:56 +0900

    Send mobile orientation to Server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var WebSocketServer = require('websocket').server;
 var http = require('http');
 var fs     = require('fs');
 
@@ -21,5 +22,38 @@ var server = http.createServer((request, response) => {
 
 server.listen(7777, () => {
   console.log((new Date()) + ' Server is listening on port 7777');
+});
+
+wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+  return true;
+}
+
+wsServer.on('request', function(request) {
+  if (!originIsAllowed(request.origin)) {
+    request.reject();
+    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    return;
+  }
+
+  var connection = request.accept('echo-protocol', request.origin);
+  console.log((new Date()) + ' Connection accepted.');
+  connection.on('message', function(message) {
+    if (message.type === 'utf8') {
+      console.log('Received Message: ' + message.utf8Data);
+      connection.sendUTF(message.utf8Data);
+    } else if (message.type === 'binary') {
+      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+      connection.sendBytes(message.binaryData);
+    }
+  });
+
+  connection.on('close', function(reasonCode, description) {
+    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+  });
 });
 
