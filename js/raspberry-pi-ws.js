c98d3d0377fbe0695a5c6b385c2d061c12477b47
kunou <t-kunou@esm.co.jp>
2018-11-21 01:03:13 +0900

    wip

--- js/raspberry-pi-ws.js
+++ js/raspberry-pi-ws.js
@@ -1,3 +1,52 @@
-const senriganSocket = new SenriganSocket(7779);
-senriganSocket.init();
+socketConnect();
 
+async function socketConnect() {
+  const senriganSocket = new SenriganSocket(7779);
+  await senriganSocket.init();
+
+  let onmessage = senriganSocket.ws.onmessage;
+  senriganSocket.ws.onmessage = ((event) => {
+    onmessage(event);
+    console.log(event)
+  });
+
+  RTCPeerConnection = window.RTCPeerConnection
+  RTCSessionDescription = window.RTCSessionDescription
+
+  let pc_config = {"iceServers":[]};
+  let peer = new RTCPeerConnection(pc_config);
+}
+
+function setOffer(sessionDescription) {
+  if (peerConnection) {
+    console.error('peerConnection alreay exist!');
+  }
+  peerConnection = prepareNewConnection();
+  peerConnection.setRemoteDescription(sessionDescription).then(function() {
+      console.log('setRemoteDescription(offer) succsess in promise');
+      makeAnswer();
+  }).catch(function(err) {
+    console.error('setRemoteDescription(offer) ERROR: ', err);
+  });
+}
+
+function makeAnswer() {
+  console.log('sending Answer. Creating remote session description...' );
+  if (! peerConnection) {
+    console.error('peerConnection NOT exist!');
+    return;
+  }
+  
+  peerConnection.createAnswer()
+  .then(function (sessionDescription) {
+    console.log('createAnswer() succsess in promise');
+    return peerConnection.setLocalDescription(sessionDescription);
+  }).then(function() {
+    console.log('setLocalDescription() succsess in promise');
+    // -- Trickle ICE の場合は、初期SDPを相手に送る -- 
+    // -- Vanilla ICE の場合には、まだSDPは送らない --
+    //sendSdp(peerConnection.localDescription);
+  }).catch(function(err) {
+    console.error(err);
+  });
+}
--- js/ws-client.js
+++ js/ws-client.js
@@ -1,31 +1,35 @@
 const SenriganSocket = function(port) {
   this.port = port;
   this.ws = null;
+  this.onmessages = [];
 }
 
 SenriganSocket.prototype = {
   init: function() {
-    ws = new WebSocket('ws://localhost:' + this.port + '/',['echo-protocol','soap', 'xmpp']);
+    return new Promise((resolve, reject) => {
+      this.ws = new WebSocket('ws://localhost:' + this.port + '/',['echo-protocol','soap', 'xmpp']);
 
-    ws.onopen = function() {
-     ws.send('hello');
-    };
+      this.ws.onopen = function() {
+        this.send('hello');
+        resolve();
+      };
 
-    ws.onerror = function (error) {
-      console.log('WebSocket Error ' + error);
-  };
+      this.ws.onerror = function (error) {
+        console.log('WebSocket Error ' + error);
+      };
 
-    ws.onmessage = function (e) {
-      console.log('Server: ' + e.data);
-    };
+      this.ws.onmessage = function (e) {
+        console.log('Server: ' + e.data);
+      };
 
-    ws.onclose = function () {
-      console.log('Server: closed');
-    };
+      this.ws.onclose = function () {
+        console.log('Server: closed');
+      };
+    });
   },
 
   sendToServer(num) {
-   ws.send(num);
+   this.ws.send(num);
   }
 }
 
